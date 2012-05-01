2720f22a500599ba4ab8b07a3a11dffaf2ff4cc4
Jevin O. Sewaruth <jevin9@gmail.com>
2012-05-01 11:25:14 +0400

    Initial import

---
+++ listen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Original script written by Stuart Colville: http://muffinresearch.co.uk/archives/2010/10/15/fake-smtp-server-with-python/
+"""A noddy fake smtp server."""
+
+import smtpd
+import asyncore
+import time
+
+class FakeSMTPServer(smtpd.SMTPServer):
+    """A Fake smtp server"""
+
+    def __init__(*args, **kwargs):
+        print "Running fake smtp server on port 25"
+        smtpd.SMTPServer.__init__(*args, **kwargs)
+
+    def process_message(*args, **kwargs):
+        mail = open("mails/"+str(time.time())+".eml", "w")
+        print "New mail from " + args[2]
+        mail.write(args[4])
+        mail.close
+        pass
+
+if __name__ == "__main__":
+    smtp_server = FakeSMTPServer(('localhost', 25), None)
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        smtp_server.close()
