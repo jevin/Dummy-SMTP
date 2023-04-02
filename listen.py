b6c69ab907a4b33f5650eff9b5ddc93026e4ad64
serenith <infocern.ml@gmail.com>
2023-04-02 18:22:16 +0100

    allow users to pass in any port from command line

--- listen.py
+++ listen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Original script written by Stuart Colville: http://muffinresearch.co.uk/archives/2010/10/15/fake-smtp-server-with-python/
 """A noddy fake smtp server."""
-
+import sys
 import smtpd
 import asyncore
 import time
@@ -10,7 +10,7 @@ class FakeSMTPServer(smtpd.SMTPServer):
     """A Fake smtp server"""
 
     def __init__(*args, **kwargs):
-        print "Running fake smtp server on port 25"
+        print "Running fake smtp server on " + str(locals()['args'][1][0]) + ":" + str(locals()['args'][1][1])
         smtpd.SMTPServer.__init__(*args, **kwargs)
 
     def process_message(*args, **kwargs):
@@ -21,7 +21,14 @@ class FakeSMTPServer(smtpd.SMTPServer):
         pass
 
 if __name__ == "__main__":
-    smtp_server = FakeSMTPServer(('localhost', 25), None)
+    port = 25
+    if len(sys.argv) == 2:
+        try:
+            port = int(sys.argv[1])
+        except:
+            print("port must be integer, unknown value("+sys.argv[1]+") provided. default port 25 will be used")
+
+    smtp_server = FakeSMTPServer(('localhost', port), None)
     try:
         asyncore.loop()
     except KeyboardInterrupt:
